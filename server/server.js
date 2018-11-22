0488fc33cd02a409cb91e7a8ef7486385a7c6e77
Ramakant4168 <ram4168@gmail.com>
2018-11-23 00:33:34 +0530

    Added update todo by id route

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 
 const { ObjectID} = require('mongodb');
+const _ = require("lodash");
 const { mongoose } = require('./db/mongoose')
 const { User } = require('./models/user')
 const { Todo } = require('./models/todo')
+
 
 
 const express = require('express');
@@ -31,7 +33,7 @@ app.get("/todos",(req,res)=>{
     Todo.find().then((todos)=>{
        res.send({todos});
     })
-    .catch((e)=>res.status(400).send(er))
+    .catch((e)=>res.status(400).send(e))
 })
 
 app.get("/todos/:id",(req,res)=>{
@@ -46,6 +48,44 @@ app.get("/todos/:id",(req,res)=>{
        if(!todo){
            res.status(404).send('No to do found with given id');
        } 
+       res.send({todo});
+    })
+    .catch((e)=>res.status(400).send(e))
+})
+
+app.delete("/todos/:id",(req,res)=>{
+    let id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(400).send("Invalid ID value");
+    }
+    Todo.findByIdAndRemove(id).then((todo)=>{ 
+       console.log('3') ; 
+       res.send(todo);
+    })
+    .catch((e)=>res.status(400).send(e))
+})
+
+app.patch("/todos/:id",(req,res)=>{
+    let id = req.params.id;
+
+    let body = _.pick(req.body,['text','completed'])
+
+    if(!ObjectID.isValid(id)){
+        res.status(400).send("Invalid ID value");
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }
+    else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id,{$set:body},{new:true}).then((todo)=>{ 
+        if(!todo){
+            res.status(404).send('No to do found with given id');
+        } 
        res.send({todo});
     })
     .catch((e)=>res.status(400).send(e))
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,14 +3,17 @@ const request = require('supertest');
 const expect = require("expect");
 const {app} = require("../server");
 const {Todo} = require("../models/todo");
-
+const { ObjectID} = require('mongodb');
 
 describe("POST /todo",()=>{
     it("should post a valid todo",(done)=>{
         let passedtext = "new todo 1";
         request(app)
         .post("/todos")
-        .send({text:passedtext})
+        .send({
+            _id:new ObjectID('5bf6f274f9ec872161b9f8a6') ,
+            text:passedtext,
+        })
         .expect(200)
         .expect((res)=>{
             expect(res.body.text).toBe(text)
@@ -43,10 +46,10 @@ describe("GET /todo",()=>{
     });
 })
 
-describe.only("GET /todo/5bf58172134ef208799b8301",()=>{
+describe("GET /todo/",()=>{
     it("should get todo by id given",(done)=>{
         request(app)
-        .get("/todo/5bf58172134ef208799b8301")
+        .get("/todo/5bf6f274f9ec872161b9f8a6")
         .expect(200)
         .expect((res)=>{
             expect(res.body.todo).toBe(Object);
@@ -61,6 +64,35 @@ describe.only("GET /todo/5bf58172134ef208799b8301",()=>{
         .expect(400)
         .expect((res)=>{
             expect(res.body).toBe("Invalid ID value");
+        })
+
+        done();
+    });
+})
+
+describe("PATCH /todo/5bf6f274f9ec872161b9f8a6",()=>{
+    
+    it("should update todo by id given",(done)=>{
+        request(app)
+        .patch("/todo/5bf6f274f9ec872161b9f8a6")
+        .send({text:"updated",complete:true})
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo).toBe(Object);
+        })
+
+        done();
+    });
+})
+
+describe("DELETE /todo/5bf6f274f9ec872161b9f8a6",()=>{
+    
+    it("should delete todo by id given",(done)=>{
+        request(app)
+        .delete("/todo/5bf6f274f9ec872161b9f8a6")
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo).toBe(Object);
         })
 
         done();
